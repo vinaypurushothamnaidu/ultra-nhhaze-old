7e1fd1d9544abd79d260b2cbd95fba3cf9be9492
Vinay Naidu <vinaynaidu@Vinays-MacBook-Pro.local>
2022-10-10 06:53:37 +0530

    using old code to train nhhaze

--- option.py
+++ option.py
@@ -13,8 +13,8 @@ parser=argparse.ArgumentParser()
 parser.add_argument('--steps',type=int,default=100000)
 parser.add_argument('--device',type=str,default='Automatic detection')
 parser.add_argument('--resume',type=bool,default=True)
-parser.add_argument('--eval_step',type=int,default=5000)
-parser.add_argument('--lr', default=0.001, type=float, help='learning rate')
+parser.add_argument('--eval_step',type=int,default=50)
+parser.add_argument('--lr', default=0.0003, type=float, help='learning rate')
 parser.add_argument('--model_dir',type=str,default='./trained_models/')
 parser.add_argument('--trainset',type=str,default='its_train')
 parser.add_argument('--testset',type=str,default='its_test')
@@ -24,7 +24,7 @@ parser.add_argument('--bs',type=int,default=16,help='batch size')
 opt = parser.parse_args()
 opt.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 model_name = opt.trainset+'_'+opt.net+'.pk'
-opt.model_dir="/kaggle/working/ultra-nhhaze-old/trained_models/" + model_name
+opt.model_dir="/kaggle/working/trained_models/" + model_name
 # opt.model_dir="./trained_models/" + model_name
 
 log_dir = 'logs/' + model_name
